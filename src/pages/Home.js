c3a517f8032b86ee4079ad20eb77573637be2820
yui-ubc <yui.mikuriya@gmail.com>
2023-03-12 01:42:24 -0800

    Created home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,24 @@
 import React from "react";
+import homeTop from "./home-top.svg";
+import './Home.css'
 
 const About = () => {
 return (
 	<div>
-	<h1>Home Page</h1>
-	</div>
+        <div>
+            <img src={homeTop}/>
+	    </div>
+        <div>
+
+        </div>
+        <div class="video-placeholder">
+            <h1 class="title">How to use it?</h1>
+            <div class="youTube" >
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/lDbSsoyje3o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+            
+        </div>
+    </div>
 );
 };
 

34ef62ac68ac3b032c89aabc350fb8c04a7b6598
yui-ubc <yui.mikuriya@gmail.com>
2023-03-11 18:21:34 -0800

    AboutUs modification

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,14 @@
 import React from "react";
+import aboutus from './aboutus.png';
+import './About.css';
   
 const Home = () => {
   return (
-    <div>
+    <div class="top-container">
       <h1>About Pinion</h1>
       <h6>Stay locally vocal</h6>
-      <div style={{ margin: '100px' }}>
-        <img src="/About-us.png" alt="react logo" style={{ width: '400px', }}/>
+      <div style={{margin: '100px' }}>
+        <img src={aboutus} alt="react logoo" width="300" height="00" />
     </div>
     </div>
   );

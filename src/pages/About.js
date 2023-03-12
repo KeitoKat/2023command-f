a3403526ce9b83722cd6f506e0d019c8280251c2
yui-ubc <yui.mikuriya@gmail.com>
2023-03-11 17:45:39 -0800

    Added the images

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,6 +4,8 @@ const Home = () => {
   return (
     <div>
       <h1>About us Page</h1>
+      <h1>About Pinion</h1>
+      <h6>Stay locally vocal</h6>
     </div>
   );
 };
--- src/pages/Map.js
+++ src/pages/Map.js
@@ -4,6 +4,7 @@ const ContactUs = () => {
 return (
 	<div>
 	<h1>Map Page</h1>
+    <iframe src="https://www.google.com/maps/d/embed?mid=1Nviv1JmRdpTNjj7BuXg06dc2uW0&ehbc=2E312F" width="640" height="480"></iframe>
 	</div>
 );
 };

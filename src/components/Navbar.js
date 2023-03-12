c1ca61b2a92ff2caa8afc222fa0e0deabdcd3533
ajackman2 <andrea59@student.ubc.ca>
2023-03-12 10:06:15 -0700

    deleted old code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -51,25 +51,3 @@ const Navbar = () => {
   )
 }
 export default Navbar
-
-// import React from 'react';
-// import {  Link } from "react-router-dom";
-// const Navbar= () =>{
-//   return (
-//   <div>
-//     <li>
-//       <Link to="/">Home</Link>
-//     </li>
-//     <li>
-//       <Link to="/About">AboutUs</Link>
-//     </li>
-//     <li>
-//       <Link to="/Map">Maps</Link>
-//     </li>
-//     <li>
-//       <Link to="/Feedback">Feedback</Link>
-//     </li>
-//   </div>
-//   );
-// }
-// export default Navbar;

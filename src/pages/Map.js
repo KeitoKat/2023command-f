9e7fd021ba2dd8e9bd8e501510019170c81a39e9
yui-ubc <yui.mikuriya@gmail.com>
2023-03-11 18:36:56 -0800

    Resolved directory to map

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-const ContactUs = () => {
+const Map = () => {
     return (
         <div>
           <h1>Map Page</h1>
+        <iframe src="https://www.google.com/maps/d/embed?mid=1Nviv1JmRdpTNjj7BuXg06dc2uW0&ehbc=2E312F" width="640" height="480"></iframe>
+
         </div>
       );
 };
 
-export default ContactUs;
+export default Map;

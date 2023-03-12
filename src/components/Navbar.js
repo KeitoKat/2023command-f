2c39d7ef88d7b699b250ee9b502cca9df11af96d
ajackman2 <andrea59@student.ubc.ca>
2023-03-11 23:51:15 -0800

    drop down box for maps

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PrimaryNav, MenuLink, Menu, Hamburger } from './NavElement'
 import myLogo from './Pinion_logo.png'
 import './Navbar.css'
 import {Link} from 'react-router-dom';
 
-
 const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+    };
   return (
     <>
       <PrimaryNav >
@@ -20,9 +24,27 @@ const Navbar = () => {
           <MenuLink to="/About" activeStyle>
             About Us
           </MenuLink>
-          <MenuLink to="/Map" activeStyle>
+          {/* <MenuLink to="/Map" activeStyle>
             Maps
-          </MenuLink>
+          </MenuLink> */}
+          <div className="dropdown">
+            <MenuLink onClick={toggleDropdown} activeStyle>
+              Maps
+            </MenuLink>
+            {isDropdownOpen && (
+              <ul className="dropdown-menu">
+                <li>
+                  <Link to="/gender-neutral">Gender Neutral Washrooms</Link>
+                </li>
+                <li>
+                  <Link to="/elevators">Elevator Map</Link>
+                </li>
+                <li>
+                  <Link to="/water-fountains">Water Fountains and Microwaves</Link>
+                </li>
+              </ul>
+            )}
+          </div>
           <MenuLink to="/Feedback" activeStyle>
             Share Feedback
           </MenuLink>
---
+++ src/pages/gender-neutral.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const genderNeutral = () => {
+return (
+	<div>
+	<h1>Gender Neutral Washroom Map</h1>
+	</div>
+);
+};
+
+export default genderNeutral;
